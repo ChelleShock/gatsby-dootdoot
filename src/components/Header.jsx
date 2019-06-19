c00b3a916af0d9920da2b83c4eb3eea4ea7f36d8
Michelle <chellshock@live.com>
2019-06-19 10:31:11 -0500

    Uncommented Video link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,32 +2,28 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Header = (props) => (
-    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
-        {/* <div className="logo">
-            <span className="icon fa-art">sDADAS</span>
-        </div> */}
-        <div className="content">
+	<header id="header" style={props.timeout ? {display: 'none'} : {}}>
 
-            <div className="inner">
-                <h1>Doot.Doot</h1>
-                <p>i dig art and music...and you, if you're not mean.</p>
-            </div>
-        </div>
-        <nav>
-            <ul>
-                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('paint') }}>Paint</a></li>
-                {/* <li><a href="javascript:;" onClick={() => { props.onOpenArticle('videos') }}>Video</a></li> */}
-
-                <li><a href="javascript:;" onClick={() => { props.onOpenArticle('print') }}>Print</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
-            </ul>
-        </nav>
-    </header>
+		<div className="content">
+			<div className="inner">
+				<h1>Doot.Doot</h1>
+				<p>i dig art and music...and you, if you're not mean.</p>
+			</div>
+		</div>
+		<nav>
+			<ul>
+				<li><a href="javascript:;" onClick={() => { props.onOpenArticle('paint') }}>Paint</a></li>
+				<li><a href="javascript:;" onClick={() => { props.onOpenArticle('videos') }}>Video</a></li>
+				<li><a href="javascript:;" onClick={() => { props.onOpenArticle('print') }}>Print</a></li>
+				<li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
+			</ul>
+		</nav>
+	</header>
 )
 
 Header.propTypes = {
-    onOpenArticle: PropTypes.func,
-    timeout: PropTypes.bool
+	onOpenArticle: PropTypes.func,
+	timeout: PropTypes.bool
 }
 
 export default Header
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -25,171 +25,172 @@ import * as emailjs from 'emailjs-com'
 
 class Main extends React.Component {
   render() {
-    let close = (
-      <div
-        className="close"
-        onClick={() => {
-          this.props.onCloseArticle()
-        }}
-      />
-    )
-    const breakpointColumnsObj = {
-      default: 3,
-      1100: 3,
-      700: 2,
-      500: 1,
-    }
-    ;(function() {
-      emailjs.init('<YOUR USER ID>')
-    })()
+	let close = (
+	  <div
+		className="close"
+		onClick={() => {
+		  this.props.onCloseArticle()
+		}}
+	  />
+	)
+	const breakpointColumnsObj = {
+	  default: 3,
+	  1100: 3,
+	  700: 2,
+	  500: 1,
+	}
+	;(function() {
+	  emailjs.init('<YOUR USER ID>')
+	})()
 
-    return (
-      <div
-        ref={this.props.setWrapperRef}
-        id="main"
-        style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}
-      >
-        <article
-          id="paint"
-          className={`${this.props.article === 'paint' ? 'active' : ''} ${
-            this.props.articleTimeout ? 'timeout' : ''
-          }`}
-          style={{ display: 'none' }}
-        >
-          <h2 className="major">Paint</h2>
+	return (
+	  <div
+		ref={this.props.setWrapperRef}
+		id="main"
+		style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}
+	  >
+		<article
+		  id="paint"
+		  className={`${this.props.article === 'paint' ? 'active' : ''} ${
+			this.props.articleTimeout ? 'timeout' : ''
+		  }`}
+		  style={{ display: 'none' }}
+		>
+		  <h2 className="major">Paint</h2>
 
-          <Masonry
-            breakpointCols={breakpointColumnsObj}
-            className="my-masonry-grid"
-            columnClassName="my-masonry-grid_column"
-          >
-            <img src={explode} alt="Blue Painting by Doot.Doot" />
+		  <Masonry
+			breakpointCols={breakpointColumnsObj}
+			className="my-masonry-grid"
+			columnClassName="my-masonry-grid_column"
+		  >
+			<img src={explode} alt="Blue Painting by Doot.Doot" />
 
-            <img src={gene} alt="Blue Painting by Doot.Doot" />
-            <img src={mural} alt="Blue Painting by Doot.Doot" />
-            <img src={nobodyDies} alt="Blue Painting by Doot.Doot" />
-            <img src={electro} alt="Blue Painting by Doot.Doot" />
-            <img src={ghost} alt="Blue Painting by Doot.Doot" />
-            <img src={phoneWoman} alt="Blue Painting by Doot.Doot" />
-            <img src={redVampire} alt="Blue Painting by Doot.Doot" />
-            <img src={betrayal} alt="Blue Painting by Doot.Doot" />
+			<img src={gene} alt="Blue Painting by Doot.Doot" />
+			<img src={mural} alt="Blue Painting by Doot.Doot" />
+			<img src={nobodyDies} alt="Blue Painting by Doot.Doot" />
+			<img src={electro} alt="Blue Painting by Doot.Doot" />
+			<img src={ghost} alt="Blue Painting by Doot.Doot" />
+			<img src={phoneWoman} alt="Blue Painting by Doot.Doot" />
+			<img src={redVampire} alt="Blue Painting by Doot.Doot" />
+			<img src={betrayal} alt="Blue Painting by Doot.Doot" />
 
-            <img src={space} alt="Blue Painting by Doot.Doot" />
-            <img src={ufo} alt="Blue Painting by Doot.Doot" />
-            <img src={yellowWoman} alt="Blue Painting by Doot.Doot" />
-            <img src={boltheartgene} alt="Blue Painting by Doot.Doot" />
-            <img src={red} alt="Blue Painting by Doot.Doot" />
-            <img src={sun} alt="Blue Painting by Doot.Doot" />
-            <img src={black} alt="Blue Painting by Doot.Doot" />
-          </Masonry>
+			<img src={space} alt="Blue Painting by Doot.Doot" />
+			<img src={ufo} alt="Blue Painting by Doot.Doot" />
+			<img src={yellowWoman} alt="Blue Painting by Doot.Doot" />
+			<img src={boltheartgene} alt="Blue Painting by Doot.Doot" />
+			<img src={red} alt="Blue Painting by Doot.Doot" />
+			<img src={sun} alt="Blue Painting by Doot.Doot" />
+			<img src={black} alt="Blue Painting by Doot.Doot" />
+		  </Masonry>
 
-          {close}
-        </article>
+		  {close}
+		</article>
 
-        <article
-          id="work"
-          className={`${this.props.article === 'videos' ? 'active' : ''} ${
-            this.props.articleTimeout ? 'timeout' : ''
-          }`}
-          style={{ display: 'none' }}
-        >
-          <h2 className="major">Videos</h2>
-          <iframe
-            width="100%"
-            height="315"
-            title="Pet Envy"
-            src="https://www.youtube.com/embed/Ow8yKBR5Dr8?rel=0"
-            frameborder="0"
-            allow="autoplay; encrypted-media"
-            allowfullscreen
-          />
-          <i className="caption">Directed, shot, and edited. </i>
+		<article
+		  id="work"
+		  className={`${this.props.article === 'videos' ? 'active' : ''} ${
+			this.props.articleTimeout ? 'timeout' : ''
+		  }`}
+		  style={{ display: 'none' }}
+		>
+		  <h2 className="major">Videos</h2>
+		  <iframe
+			width="100%"
+			height="315"
+			title="Pet Envy"
+			src="https://www.youtube.com/embed/Ow8yKBR5Dr8?rel=0"
+			frameborder="0"
+			allow="autoplay; encrypted-media"
+			allowfullscreen
+		  />
+		  <i className="caption">Directed, shot, and edited. </i>
 
-          <iframe
-            width="100%"
-            height="315"
-            title="video2"
-            src="https://www.youtube.com/embed/jctDeZlXbPU?rel=0"
-            frameborder="0"
-            allow="autoplay; encrypted-media"
-            allowfullscreen
-          />
-          <i className="caption">Captured, shot, and edited. </i>
+		  <iframe
+			width="100%"
+			height="315"
+			title="video2"
+			src="https://www.youtube.com/embed/jctDeZlXbPU?rel=0"
+			frameborder="0"
+			allow="autoplay; encrypted-media"
+			allowfullscreen
+		  />
+		  <i className="caption">Captured, shot, and edited. </i>
 
-          <iframe
-            width="100%"
-            height="315"
-            title="video3"
-            src="https://www.youtube.com/embed/GBAOMYSBz9Y?rel=0"
-            frameborder="0"
-            allow="autoplay; encrypted-media"
-            allowfullscreen
-          />
-          <i className="caption">Directed, shot, and edited. </i>
+		  <iframe
+			width="100%"
+			height="315"
+			title="video3"
+			src="https://www.youtube.com/embed/GBAOMYSBz9Y?rel=0"
+			frameborder="0"
+			allow="autoplay; encrypted-media"
+			allowfullscreen
+		  />
+		  <i className="caption">Directed, shot, and edited. </i>
 
-          <iframe
-            width="100%"
-            height="315"
-            title="video4"
-            src="https://www.youtube.com/embed/ZbCY1lprfCU?rel=0"
-            frameborder="0"
-            allow="autoplay; encrypted-media"
-            allowfullscreen
-          />
-          <i className="caption">Directed, shot, and edited. </i>
+		  <iframe
+			width="100%"
+			height="315"
+			title="video4"
+			src="https://www.youtube.com/embed/ZbCY1lprfCU?rel=0"
+			frameborder="0"
+			allow="autoplay; encrypted-media"
+			allowfullscreen
+		  />
+		  <i className="caption">Directed, shot, and edited. </i>
 
-          <iframe
-            width="100%"
-            height="315"
-            title="video5"
-            src="https://www.youtube.com/embed/90b-AWqD2tU?rel=0"
-            frameborder="0"
-            allow="autoplay; encrypted-media"
-            allowfullscreen
-          />
-          <i className="caption">Directed, shot, and edited. </i>
+		  <iframe
+			width="100%"
+			height="315"
+			title="video5"
+			src="https://www.youtube.com/embed/90b-AWqD2tU?rel=0"
+			frameborder="0"
+			allow="autoplay; encrypted-media"
+			allowfullscreen
+		  />
+		  <i className="caption">Directed, shot, and edited. </i>
 
-          {close}
-        </article>
-        <article
-          id="print"
-          className={`${this.props.article === 'print' ? 'active' : ''} ${
-            this.props.articleTimeout ? 'timeout' : ''
-          }`}
-          style={{ display: 'none' }}
-        >
-          <h2 className="major">Print</h2>
+		  {close}
+		</article>
+		<article
+		  id="print"
+		  className={`${this.props.article === 'print' ? 'active' : ''} ${
+			this.props.articleTimeout ? 'timeout' : ''
+		  }`}
+		  style={{ display: 'none' }}
+		>
+		  <h2 className="major">Print</h2>
 
-          <p>Print work coming soon.</p>
+		  <p>Print work coming soon.</p>
 
-          {close}
-        </article>
+		  {close}
+		</article>
 
-        <article
-          id="contact"
-          className={`${this.props.article === 'contact' ? 'active' : ''} ${
-            this.props.articleTimeout ? 'timeout' : ''
-          }`}
-          style={{ display: 'none' }}
-        >
-          <h2 className="major">Contact</h2>
-          <NameForm />
+		<article
+		  id="contact"
+		  className={`${this.props.article === 'contact' ? 'active' : ''} ${
+			this.props.articleTimeout ? 'timeout' : ''
+		  }`}
+		  style={{ display: 'none' }}
+		>
+		  <h2 className="major">Contact</h2>
+		  <NameForm />
 
-          <ul className="icons">
-            <li>
-              <a
-                href="https://www.instagram.com/doot.dootmedia/"
-                target="_blank"
-                className="icon fa-instagram"
-              >
-                <span className="label">Instagram</span>
-              </a>
-            </li>
-          </ul>
-          {close}
-        </article>
-      </div>
-    )
+		  <ul className="icons">
+			<li>
+			  <a
+				href="https://www.instagram.com/doot.dootmedia/"
+				target="_blank"
+				className="icon fa-instagram"
+				rel="noopener noreferrer"
+			  >
+				<span className="label">Instagram</span>
+			  </a>
+			</li>
+		  </ul>
+		  {close}
+		</article>
+	  </div>
+	)
   }
 }
 
